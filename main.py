2cfddcb60e3c33c21cc98a8c5bb9b355a3461a84
Luca Wrabetz <wrabetzluca@gmail.com>
2024-05-20 14:33:45 -0400

    Initialize models, add variables.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gurobipy import *
+from models import *
 
 # Data
 # FLInstanceGenerator(n, T): A class to generate random instances, and construct 
@@ -11,17 +12,32 @@ from gurobipy import *
 # x_0, x_1, ..., x_T: points in R^n 
 # Gamma: model parameter (float)
 
-# MIP
-# Problem:
-# input: x_0, x_1, ..., x_T, n, Gamma, (T) (T is total time periods oracle known)
+class OfflineMIP:
+    def __init__(self) -> None:
+        '''Constructs blank gurobi model.'''
+        self.model = Model("OfflineMIP")
+        self.zeta = None
+        self.z = None
+        self.y = None
 
+    def configure_model(self, instance: FLFullInstance) -> None:
+        '''
+        Set up variables, constraints, and objective function given instance.
+        '''
+        # Add variables:
+        self.y = [[self.model.addVar(vtype=GRB.BINARY, name=f"y^{t}_{i}") for i in range(1, t + 1)] for t in range(1, instance.T + 1)]
+        self.z = [[[self.model.addVar(vtype=GRB.BINARY, name=f"z^{t}_{i}_{j}") for j in range(0, t + 1)] for i in range(1, t + 1)] for t in range(1, instance.T + 1)]
+        self.zeta = [self.model.addVar(vtype=GRB.CONTINUOUS, name=f"zeta^{t}") for t in range(1, instance.T + 1)]
 
-# Competitive Algorithm
-# Problem: 
-# input: x_0, x_1, ..., t, n, Gamma, (t) (t is current time)
+    def write_model(self, filename: str = "OfflineMIP.lp") -> None:
+        '''Write model to file.'''
+        self.model.write(filename)
 
 def main():
-    print("Hello World!")
+    instance = FLFullInstance()
+    mip = OfflineMIP()
+    mip.configure_model(instance)
+    mip.write_model("test.lp")
 
 if __name__ == '__main__':
     main()
