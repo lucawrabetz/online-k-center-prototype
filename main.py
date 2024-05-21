b7d3d8a10e5415aa09cc9edaaea6056312cf2aa0
Luca Wrabetz <wrabetzluca@gmail.com>
2024-05-21 16:02:11 -0400

    Build basic random generator mixin for instance class.

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ class OfflineMIP:
         Set up variables, constraints, and objective function given instance.
         '''
         # Add variables:
-        self.y = [[self.model.addVar(vtype=GRB.BINARY, name=f"y^{t}_{i}") for i in range(1, t + 1)] for t in range(1, instance.T + 1)]
-        self.z = [[[self.model.addVar(vtype=GRB.BINARY, name=f"z^{t}_{i}_{j}") for j in range(0, t + 1)] for i in range(1, t + 1)] for t in range(1, instance.T + 1)]
-        self.zeta = [self.model.addVar(vtype=GRB.CONTINUOUS, name=f"zeta^{t}") for t in range(1, instance.T + 1)]
+        self.y = [[self.model.addVar(vtype=GRB.BINARY, name=f"y^{t}_{i}") for i in range(1, t + 1)] for t in range(1, instance.shape.T + 1)]
+        self.z = [[[self.model.addVar(vtype=GRB.BINARY, name=f"z^{t}_{i}_{j}") for j in range(0, t + 1)] for i in range(1, t + 1)] for t in range(1, instance.shape.T + 1)]
+        self.zeta = [self.model.addVar(vtype=GRB.CONTINUOUS, name=f"zeta^{t}") for t in range(1, instance.shape.T + 1)]
 
     def write_model(self, filename: str = "OfflineMIP.lp") -> None:
         '''Write model to file.'''
         self.model.write(filename)
 
 def main():
-    instance = FLFullInstance()
+    instance = FLFullInstance(INSTANCE_SHAPES["test"])
     mip = OfflineMIP()
     mip.configure_model(instance)
     mip.write_model("test.lp")
--- models.py
+++ models.py
@@ -1,10 +1,73 @@
-class FLFullInstance:
-    def __init__(self):
-        self.n: int = 1
-        self.T: int = 1
-        self.x: List[List[float]] = [[0.0]]
-        self.Gamma: float = 20.0
+import numpy as np
 
-class FLInstanceDistributor:
-    def __init__(self, full_instance):
-        self.full_instance = full_instance
+class DPoint:
+    def __init__(self, x: np.ndarray) -> None:
+        self.x: np.ndarray = np.asarray(x)
+        self.n: int = len(x)
+
+class FLInstanceShape:
+    def __init__(self, n: int = 2, T: int = 3, Gamma: float = 10.0) -> None:
+        self.n: int = n
+        self.T: int = T
+        self.Gamma: np.float64 = np.float64(Gamma)
+
+    def print(self) -> None:
+        print(f"n: {self.n}")
+        print(f"T: {self.T}")
+
+INSTANCE_SHAPES = {
+    "test": FLInstanceShape(),
+}
+
+class FLDistribution:
+    '''
+    Class to hold "distributions" for generating demand points for FLFullInstances.
+    '''
+    def __init__(self, shape: FLInstanceShape) -> None:
+        self.shape: FLInstanceShape = shape
+        self.rng: np.random.Generator = np.random.default_rng()
+
+    def unit_square(self) -> np.ndarray:
+        '''
+        Generate points uniformly at random from the unit square of dimension n.
+        '''
+        return np.asarray([DPoint(self.rng.random((self.shape.n,))) for _ in range(self.shape.T + 1)])
+        
+
+
+class FLFullInstance:
+    def __init__(self, shape: FLInstanceShape) -> None:
+        self.shape: FLInstanceShape = shape
+        self.x: np.ndarray = np.asarray([DPoint(np.zeros(self.shape.n)) for _ in range(self.shape.T + 1)])
+
+    def set_x_random(self) -> None:
+        '''
+        Set demand points by instantiating and calling on an FLDistribution object.
+        '''
+        dist = FLDistribution(self.shape)
+        self.x = dist.unit_square()
+        print("Set x randomly.")
+        self.print()
+
+    def read_x_from_file(self, filename: str) -> None:
+        '''
+        Read demand points from file.
+        '''
+        pass
+
+    def print(self) -> None:
+        print(f"n: {self.shape.n}")
+        print(f"T: {self.shape.T}")
+        print(f"Gamma: {self.shape.Gamma}")
+        for t in range(self.shape.T + 1):
+            print(f"x_{t}: {self.x[t].x}")
+
+# class OnlineWrapper (?)
+
+def main():
+    instance = FLFullInstance(INSTANCE_SHAPES["test"])
+    instance.print()
+    instance.set_x_random()
+
+if __name__ == '__main__':
+    main()
